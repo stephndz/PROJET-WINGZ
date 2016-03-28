049b6285d423fb8280f38b327f8343a108c7b35b
Mugisha David <david.mugisha@supelec.fr>
2016-03-28 04:20:03 +0200

    Corrected issue #3, the up button works properly Setted default logo ref #1, the final is yet to be produced @stephndz

--- app/src/main/java/com/wingz/core/activity/ScrollingActivity.java
+++ app/src/main/java/com/wingz/core/activity/ScrollingActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2016. The Wingz Project
+ * Developed by Wingz Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.wingz.core.activity;
 
 import android.Manifest;
@@ -81,7 +98,7 @@ public class ScrollingActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_scrolling);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -309,7 +326,9 @@ public class ScrollingActivity extends AppCompatActivity
     public void onConnectionFailed(ConnectionResult result) {
         // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
         // onConnectionFailed.
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+        if(result != null){
+            Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+        }
     }
 
     /**
